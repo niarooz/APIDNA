47c5a9c24f1fcb56f3084336106613fa3e65d73d
Niaz Ekeke <razb@Niazs-MacBook-Pro.local>
2022-04-15 15:58:09 -0500

    Apr 15 Update Add_device Method

--- dnaAPI.py
+++ dnaAPI.py
@@ -8,6 +8,7 @@ class CiscoDNA:
         self.UrlExtend = ""
         self.header = {"Content-Type": "application/json"}
         self.token = ""
+        self.taskID= ""
 
     def get_token(self, urlExtend):
         self.UrlExtend = urlExtend
@@ -16,7 +17,7 @@ class CiscoDNA:
             self.token = response.json()["Token"]
             return self.token
         else:
-            print("Failed with status {}".format(response.status_code))
+            print("Failed to get token: {}".format(response.status_code))
             return
     def get_devices(self, urlExtend):
         self.UrlExtend = urlExtend
@@ -25,7 +26,43 @@ class CiscoDNA:
         if response.ok:
             return response.json()
         else:
-            print("Failed with status: {}".format(response.status_code))
+            print("\nFailed to get device list: {}".format(response.status_code))
             print("Body: {}".format(response.text))
             return
+
+    def add_device(self, urlExtend, device):
+        self.UrlExtend = urlExtend
+        self.header = {"Content-Type": "application/json", "X-Auth-Token": self.token}
+        response = requests.post(f"{self.baseUrl}{self.UrlExtend}", json=device, headers=self.header)
+        if response.ok:
+            self.taskID = response.json()["response"]["taskid"]
+            return response.json()
+        else:
+            print("\nFailed to add device: {}".format(response.status_code))
+            print("Body: {}".format(response.text))
+            return
+
+    def get_taskStatus(self, urlExtend):
+        self.UrlExtend = urlExtend
+        self.header = {"Content-Type": "application/json", "X-Auth-Token": self.token}
+        response = requests.post(f"{self.baseUrl}{self.UrlExtend}{self.taskID}", headers=self.header)
+        if response.ok:
+            while response.json()["response"]["isError"]:
+                response.json()["response"]["progress"]
+                print("Waiting for response...")
+            print("device added successfully")
+            return
+        else:
+            print("\nFailed to retrieve status: {}".format(response.status_code))
+            print("Body: {}".format(response.text))
+            return
+
+    # def del_device(self, urlExtend, devID):
+    #     self.UrlExtend = urlExtend
+    #     response = requests.post(f"{self.baseUrl}{self.UrlExtend}{self.taskID}", headers=self.header)
+    #     if response.ok:
+    #         pass
+
+
+
 
--- main.py
+++ main.py
@@ -6,4 +6,22 @@ if __name__ == "__main__":
     # print(token)
 
     device_list = Niaz.get_devices("/dna/intent/api/v1/network-device")
-    print(json.dumps(device_list, indent=4))
+    for device in device_list["response"]:
+        print(f"Device ID: {device['id']}  Device IP: {device['managementIpAddress']}")
+
+    new_device = {
+        "ipAddress": ["192.0.2.1"],
+        "snmpVersion": "v2",
+        "snmpROCommunity": "readonly",
+        "snmpRWCommunity": "readwrite",
+        "snmpRetry": "1",
+        "snmpTimeout": "60",
+        "cliTransport": "ssh",
+        "userName": "nick",
+        "password": "secret123!",
+        "enablePassword": "secret456!",
+    }
+
+    Niaz.add_device("/dna/intent/api/v1/network-device", new_device)
+
+
